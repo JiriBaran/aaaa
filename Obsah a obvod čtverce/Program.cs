e8f75fe4b9ea9c6b43f83e200f2ee929f1153d6b
Jiří Baran <jurka.baran@seznam.cz>
2020-03-30 17:22:21 +0200

    Nic

---
+++ Obsah a obvod čtverce/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obsah_a_obvod_čtverce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double strana = 0;
+
+            Console.Write("Zadejte stranu: ");
+            strana = double.Parse(Console.ReadLine());
+
+            double obvod = 4 * strana;
+            double obsah = strana * strana;
+
+            Console.WriteLine("Obvod je: {0}", obvod);
+            Console.WriteLine("Obsah je: {0}", obsah);
+            Console.ReadKey();
+        }
+    }
+}
